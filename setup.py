f87eda14d91b672279c8e48bc325f578c075dda7
jpardobl <jpardobl@linux.eu-west-1.compute.internal>
2018-11-26 17:52:20 +0100

    Added version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages
 
 setup(
     name='pandavro',
-    version='1.1.1',
-    description='The interface between Avro and pandas DataFrame',
-    url='https://github.com/ynqa/pandavro',
-    author='Makoto Ito',
-    author_email='un.pensiero.vano@gmail.com',
+    version='1.2.0',
+    description='The interface between Avro and pandas DataFrame(fork from https://github.com/ynqa/pandavro)',
+    url='https://github.com/jpardobl/pandavro',
+    author='Javier Pardo Blasco',
+    author_email='jpard@digitalhigh.es',
     license='MIT',
     packages=find_packages(exclude=['example']),
     install_requires=[
